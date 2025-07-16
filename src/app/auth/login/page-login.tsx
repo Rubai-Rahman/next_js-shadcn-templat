1cd294eb07466a137eed7e4f6a46c7a6fa42e9b2
Rubai Rahman <rubairahman1@gmail.com>
2025-07-17 01:48:07 +0600

    initial sturucture added

---
+++ src/app/auth/login/page-login.tsx
@@ -0,0 +1,2 @@
+
+// use client side code here api call or any other client side code
---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,10 @@
+// import { Metadata } from 'next';
+// import LoginPage from './page-login';
+
+// export const metadata: Metadata = {
+//   title: 'Fluent - Login',
+// };
+
+// export default async function Page() {
+//   return <LoginPage />;
+// }
